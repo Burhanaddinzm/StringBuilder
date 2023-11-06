a3e73309e4085546466ddce788f07c0e79235db5
Burhanaddinzm <burhanaddinzm@code.edu.az>
2023-11-06 23:15:16 +0400

    String Builder practice

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+StringBuilder sb = new StringBuilder("Random Text");
+StringBuilder sb2 = new StringBuilder("More stuff that is very important", 256);
+Console.WriteLine("Capacity :{0}", sb2.Capacity);
+Console.WriteLine("Length :{0}", sb.Length);
+
+sb2.AppendLine("\nMore important text");
+string bestCust = "Bob Smith";
+sb2.AppendFormat("Best Customer : {0}", bestCust);
+Console.WriteLine(sb2.ToString());
+
+sb2.Replace("text", "characters");
+Console.WriteLine(sb2.ToString());
+sb2.Clear();
+
+sb2.Append("Random Text");
+Console.WriteLine(sb.Equals(sb2));
+sb2.Insert(11, " that's great");
+Console.WriteLine(sb2.ToString());
+
+sb2.Remove(11, 7);
+Console.WriteLine(sb2.ToString());
